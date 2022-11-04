bd71a889b5694593b860a416bd22d25948238cc6
alexcas20 <>
2022-11-03 22:14:07 -0600

    logout
    Se agrego boton  y funcion de logout

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ruta: Router) { }
 
   ngOnInit(): void {
+
+    this.checkLocalStorage();
+  }
+
+ 
+
+  checkLocalStorage(){
+    if(!localStorage.getItem('token')){
+      this.ruta.navigate(['login'])
+    }
+  }
+
+  salir(){
+    localStorage.removeItem('token');
+    this.ngOnInit();
   }
 
 }
