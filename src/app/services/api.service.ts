8f563230628cd87730f2469fdc3a8b4c7175d2b9
Hector Juvenal <hector.herrera@linkthinks.com>
2022-11-10 12:15:08 -0600

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,12 +7,12 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
-  private url:string = 'http://127.0.0.1:5000/lab/'
+  private url:string = 'http://127.0.0.1:5000/lab'
 
   constructor(private http:HttpClient) { }
 
   serviceGetUsers():Observable<any>{
-    let direccion = `${this.url}/users`;
+    let direccion = `${this.url}/allUsers`;
     return this.http.get(direccion);
   }
 }
