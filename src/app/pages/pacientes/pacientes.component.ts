85cffefe57a987388708c9e73d93a63db331cdb1
alexcas20 <alexcalvillosalas@gmail.com>
2022-11-14 22:06:37 -0600

    se agrego tabla material pacientes

--- src/app/pages/pacientes/pacientes.component.ts
+++ src/app/pages/pacientes/pacientes.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-pacientes',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pacientes.component.css']
 })
 export class PacientesComponent implements OnInit {
+  displayedColumns: string[] = ['code', 'user', 'tel', 'info', 'examenes'];
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+
+  applyFilter(event : Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
+
+    if(this.dataSource.paginator){
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
